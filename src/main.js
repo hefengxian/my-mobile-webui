f090cb712ec0b25a116a894bbeda5b7ce03ed34a
hfx <jssmith883@gmail.com>
2019-06-18 16:18:08 +0800

    加入 FastClick 解决 300ms 延时问题

--- src/main.js
+++ src/main.js
@@ -2,45 +2,46 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/router'
 import {store} from './store/store'
-import './registerServiceWorker'
+import './registerServiceWorker'        // PWA SW
 import LocalStore from './plugin/local-store/'
 import Api from './api/'
 import {isTokenValid} from './util/assist'
 import moment from 'moment'
+import FastClick from 'fastclick' // 引入插件
+
+// 处理 300ms 点击时间延时
+FastClick.attach(document.body)
 
 // 新版的 Moment.js 需要手动导入地区
 moment.locale('zh-cn')
 Vue.prototype.$moment = moment
 
 Vue.config.productionTip = false
-
 Vue.use(LocalStore)
 Vue.use(Api)
 
 // 全局路由过滤（先于 App.vue）
 router.beforeEach((to, from, next) => {
-  // app
-  // console.log('router.beforeEach', LocalStore.store.getItem(LocalStore.keys.OAUTH_KEY))
-  const token = LocalStore.store.getItem(LocalStore.keys.OAUTH_KEY)
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    let isLogin = isTokenValid(token)
-    if (!isLogin) {
-      next({
-        path: '/login',
-        query: { redirect: to.fullPath }
-      })
+    const token = LocalStore.store.getItem(LocalStore.keys.OAUTH_KEY)
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        let isLogin = isTokenValid(token)
+        if (!isLogin) {
+            next({
+                path: '/login',
+                query: {redirect: to.fullPath}
+            })
+        } else {
+            next()
+        }
     } else {
-      next()
+        next() // 确保一定要调用 next()
     }
-  } else {
-    next() // 确保一定要调用 next()
-  }
 })
 
 
 const app = new Vue({
-  store,
-  router,
-  render: h => h(App)
+    store,
+    router,
+    render: h => h(App)
 })
 app.$mount('#app')
